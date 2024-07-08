c2abf337fdb72ed7b6622cfd61c235fef37a1963
Hasan Al <Hasanalghanim@gmail.com>
2024-07-08 13:48:57 -0600

    db update

--- wolfhouse/settings.py
+++ wolfhouse/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'wolfhouse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'myappdb',
+            'USER': 'hasan',
+            'PASSWORD': 'Nadiasimmer1',
+            'HOST': 'hasanalghanim-3935.postgres.pythonanywhere-services.com',
+            'PORT': 13935
+,
     }
 }
 
