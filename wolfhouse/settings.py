c8d67664f992415705d8c43b7acffdca37ef2b0c
Hasan Al <Hasanalghanim@gmail.com>
2025-03-06 15:16:10 -0700

    Update settings.py

--- wolfhouse/settings.py
+++ wolfhouse/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'webhook',
-    'events'
+    'events',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
@@ -92,25 +93,37 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wolfhouse.wsgi.application'
 
+CKEDITOR_UPLOAD_PATH = "uploads/"
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Full',  # You can choose 'Basic', 'Full', etc.
+        'width': '100%',
+        'height': 300,
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'myappdb',
+            'USER': 'hasan',
+            'PASSWORD': 'Nadiasimmer1',
+            'HOST': 'hasanalghanim-3935.postgres.pythonanywhere-services.com',
+            'PORT': 13935
+,
     }
-#     'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'myappdb',
-#             'USER': 'hasan',
-#             'PASSWORD': 'Nadiasimmer1',
-#             'HOST': 'hasanalghanim-3935.postgres.pythonanywhere-services.com',
-#             'PORT': 13935
-# ,
-#     }
 }
+
+
+
+
 
 
 # Password validation
