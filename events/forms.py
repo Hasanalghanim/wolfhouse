245bbe05b3ca67cc8a0a5317af48cf588dca2355
Hasan Al <Hasanalghanim@gmail.com>
2025-03-13 14:33:12 -0600

    update to Add lbs form

--- events/forms.py
+++ events/forms.py
@@ -4,4 +4,4 @@ from .models import Participant
 class ParticipantForm(forms.ModelForm):
     class Meta:
         model = Participant
-        fields = ['first_name', 'last_name', 'gender', 'date_of_birth', 'school_name', 'email', 'years_of_training', 'event']
+        fields = ['first_name', 'last_name', 'gender', 'date_of_birth', 'school_name', 'email', 'years_of_training', 'event','weight']
