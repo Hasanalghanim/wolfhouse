76b4e8c3ad43b83e9f363a0e58be7ba603ac6740
Hasan Al <Hasanalghanim@gmail.com>
2025-05-02 10:16:04 -0600

    Update settings.py

--- wolfhouse/settings.py
+++ wolfhouse/settings.py
@@ -114,19 +114,19 @@ CKEDITOR_CONFIGS = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'myappdb',
+            'USER': 'hasan',
+            'PASSWORD': 'Nadiasimmer1',
+            'HOST': 'hasanalghanim-3935.postgres.pythonanywhere-services.com',
+            'PORT': 13935
+,
     }
-#     'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'myappdb',
-#             'USER': 'hasan',
-#             'PASSWORD': 'Nadiasimmer1',
-#             'HOST': 'hasanalghanim-3935.postgres.pythonanywhere-services.com',
-#             'PORT': 13935
-# ,
-#     }
 }
 
 
